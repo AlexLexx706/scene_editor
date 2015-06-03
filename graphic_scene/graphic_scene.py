585b2d0b879895ed8902c44edb2284227da6926e
AlexLexx <alexlexx1@gmail.com>
2015-06-04 01:54:54 +0300

    Сделал сетку

--- graphic_scene/graphic_scene.py
+++ graphic_scene/graphic_scene.py
@@ -5,13 +5,6 @@ import math
 
 class GraphicScene(QtGui.QGraphicsScene):
     left_button_clicked = QtCore.pyqtSignal(QtGui.QGraphicsSceneMouseEvent)
-    GRID_SIZE = 20
-
-    GRID_PEN = QtGui.QPen(QtGui.QBrush(QtCore.Qt.black), 1)
-    GRID_PEN.setCosmetic(True)
-
-    GRID_PEN_CENTER = QtGui.QPen(QtGui.QBrush(QtCore.Qt.red), 2)
-    GRID_PEN_CENTER.setCosmetic(True)
 
     def __init__(self, parent=None):
         QtGui.QGraphicsScene.__init__(self, parent)
@@ -20,38 +13,3 @@ class GraphicScene(QtGui.QGraphicsScene):
         if mouseEvent.button() == 1:
             self.left_button_clicked.emit(mouseEvent)
         QtGui.QGraphicsScene.mousePressEvent(self, mouseEvent)
-
-    def drawBackground__(self, painter, rect):
-        painter.setPen(self.GRID_PEN)
-
-        greed_step = rect.width() / painter.window().width() * self.GRID_SIZE
-        left = rect.left() - math.frexp(rect.left() / greed_step)[0] * greed_step
-        #greed_step = self.GRID_SIZE
-        #left = rect.left() - int(rect.left()) % greed_step
-
-        while left < rect.right():
-            if left == 0:
-                painter.save()
-                painter.setPen(self.GRID_PEN_CENTER)
-                painter.drawLine(left, rect.top(), left, rect.bottom())
-                painter.restore()
-            else:
-                painter.drawLine(left, rect.top(), left, rect.bottom())
-            painter.drawText(left, 0, str(left))
-            left += greed_step
-
-        greed_step = rect.height() / painter.window().height() * self.GRID_SIZE
-        top = rect.top() - math.frexp(rect.top() / greed_step)[0] * greed_step
-        #greed_step = self.GRID_SIZE
-        #top = rect.top() - int(rect.top()) % greed_step
-
-        while top < rect.bottom():
-            if top == 0:
-                painter.save()
-                painter.setPen(self.GRID_PEN_CENTER)
-                painter.drawLine(rect.left(), top, rect.right(), top)
-                painter.restore()
-            else:
-                painter.drawLine(rect.left(), top, rect.right(), top)
-            painter.drawText(0, top, str(top))
-            top += greed_step
--- graphic_scene/graphic_view.py
+++ graphic_scene/graphic_view.py
@@ -2,38 +2,100 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtCore, QtGui
 import math
+import resourses
 
 class GraphView(QtGui.QGraphicsView):
-    def __init__(self, parent=None):
-        super(GraphView, self).__init__(parent)
-        self.setCacheMode(QtGui.QGraphicsView.CacheBackground)
-        self.setViewportUpdateMode(QtGui.QGraphicsView.BoundingRectViewportUpdate)
-        self.setRenderHint(QtGui.QPainter.Antialiasing)
+    left_button_clicked = QtCore.pyqtSignal(QtGui.QGraphicsSceneMouseEvent)
+    
+    #настройки сетки
+    GRID_STEP = 10
+    GRID_PIXEL_STEP = 40
+    GRID_PEN = QtGui.QPen(QtGui.QBrush(QtCore.Qt.black), 1)
+    GRID_PEN.setCosmetic(True)
+    GRID_CENTER_PEN = QtGui.QPen(QtGui.QBrush(QtCore.Qt.red), 2)
+    GRID_CENTER_PEN.setCosmetic(True)
+    GRID_FONT = QtGui.QFont("Times", 10, True)
+    
+    GRID_BACKGROUD_BRUSH = QtGui.QBrush(QtCore.Qt.white)
 
-        #self.setTransformationAnchor(QtGui.QGraphicsView.AnchorUnderMouse)
-        #self.setResizeAnchor(QtGui.QGraphicsView.AnchorViewCenter)
-        #self.setDragMode(QtGui.QGraphicsView.ScrollHandDrag)
+    def __init__(self, parent=None):
+        '''Инициализация'''
+        super(GraphView, self).__init__(parent)
 
     def wheelEvent(self, event):
+        '''Реализация масштабирования'''
         self.scaleView(math.pow(2.0, -event.delta() / 240.0))
 
     def scaleView(self, scaleFactor):
+        '''масштабирование'''
         factor = self.matrix().scale(scaleFactor, scaleFactor).mapRect(QtCore.QRectF(0, 0, 1, 1)).width()
 
         if factor < 0.07 or factor > 100:
             return
         self.scale(scaleFactor, scaleFactor)
     
-    # def mouseMoveEvent(self, event):
-        # if event.buttons() & QtCore.Qt.MidButton:
-            # d = (event.pos() - self.s_pos) * 100
-            # self.s_pos = event.pos()
-            # #d = self.mapFromScene(event.pos() - self.s_pos)
-            # print d.x(), d.y()
-            # self.translate(d.x(), d.y())
-        # super(GraphView, self).mouseMoveEvent(event)
-    
-    # def mousePressEvent(self, event):
-        # if event.button() == QtCore.Qt.MidButton:
-            # self.s_pos = event.pos()
-        # super(GraphView, self).mousePressEvent(event)
+    def drawBackground(self, painter, rect):
+        '''Рисует сетку'''
+        rect = QtCore.QRectF(rect.x() - self.GRID_PIXEL_STEP, rect.y() - self.GRID_PIXEL_STEP,
+                        rect.width() + self.GRID_PIXEL_STEP * 2, rect.height() + self.GRID_PIXEL_STEP * 2)
+
+        scene_rect = self.mapToScene(self.viewport().geometry()).boundingRect()
+        
+        #расчёт шага по горизонтали
+        greed_step = scene_rect.width() / painter.window().width() * self.GRID_PIXEL_STEP
+        greed_step -= math.modf(greed_step / self.GRID_STEP)[0] * self.GRID_STEP
+
+        if greed_step < self.GRID_STEP:
+            greed_step = self.GRID_STEP
+
+        left = rect.left() - math.modf(rect.left() / greed_step)[0] * greed_step
+
+        text_list = []
+        painter.setPen(self.GRID_PEN)
+        
+        while left <= rect.right():
+            if int(left) == 0:
+                painter.save()
+                painter.setPen(self.GRID_CENTER_PEN)
+                painter.drawLine(left, rect.top(), left, rect.bottom())
+                painter.restore()
+            else:
+                painter.drawLine(left, rect.top(), left, rect.bottom())
+
+                p = self.mapFromScene(left, 0)
+                p.setY(p.y() - 2)
+                text_list.append((p, left))
+
+            left += greed_step
+
+        #расчёт шага по вертикали
+        greed_step = scene_rect.height() / painter.window().height() * self.GRID_PIXEL_STEP
+        greed_step -= math.modf(greed_step / self.GRID_STEP)[0] * self.GRID_STEP
+
+        if greed_step < self.GRID_STEP:
+            greed_step = self.GRID_STEP
+
+        top = rect.top() - math.modf(rect.top() / greed_step)[0] * greed_step
+
+        while top < rect.bottom():
+            if int(top) == 0:
+                painter.save()
+                painter.setPen(self.GRID_CENTER_PEN)
+                painter.drawLine(rect.left(), top, rect.right(), top)
+                painter.restore()
+            else:
+                painter.drawLine(rect.left(), top, rect.right(), top)
+            
+                p = self.mapFromScene(0, top)
+                p.setX(p.x() + 2)
+                text_list.append((p, top))
+
+            top += greed_step
+
+        #отображение не масштабируемого текста
+        painter.resetTransform()
+        painter.setFont(self.GRID_FONT)
+        
+        for pos, value in text_list:
+            painter.drawText(pos, str(int(value)))
+        
